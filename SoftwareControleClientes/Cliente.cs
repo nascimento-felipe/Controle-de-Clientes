7c1d332098b03779f2fbbe5213f2ceb0e08de926
Felipe <nascimentofelipe565@gmail.com>
2021-10-01 21:53:01 -0300

    Adicionar arquivos de projeto.

---
+++ SoftwareControleClientes/Cliente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftwareControleClientes
+{
+    class Cliente
+    {
+        private String nome;
+        private int idade;
+        private int codigo;
+        private String senha;
+
+
+        public void setIdade(int idade)
+        {
+            if (idade <= 0)
+            {
+                this.idade = 0;
+                Console.WriteLine("Digite uma idade válida.");
+            }
+            else
+            {
+                this.idade = idade;
+            }
+        }
+        public int getIdade() 
+        {
+            return idade; 
+        }
+        public void setCodigo(int id)
+        { 
+            codigo = id; 
+        }
+        public int getCodigo()
+        {
+            return codigo;
+        }
+
+        public string Nome { get => nome; set => nome = value; }
+        public string Senha { get => senha; set => senha = value; }
+    }
+}
---
+++ SoftwareControleClientes/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace SoftwareControleClientes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = DateTime.Now.Hour;
+            String[] clientes;
+            int opcao;
+
+            Cliente novoCliente = new Cliente();
+
+            // Controle da mensagem de boas vindas.
+            SendWelcomeMessage(hour);
+
+            do
+            {
+                Console.WriteLine("  Escolha uma opção:\n"
+                    + "    1. Cadastrar novo cliente\n"
+                    + "    2. Ver todos os clientes\n"
+                    + "    3. Deletar Clienten\n"
+                    + "    4. Alterar Cliente\n"
+                    + "   -1. Sair.");
+
+                opcao = int.Parse(Console.ReadLine());
+                if (opcao != -1)
+                {
+
+                }
+            } while (opcao != -1);
+        }
+
+        static void SendWelcomeMessage(int hour)
+        {
+            if (hour >= 4 && hour <= 12)
+            {
+                Console.WriteLine("  Bom Dia!");
+            }
+            else if (hour >= 12 && hour <= 18)
+            {
+                Console.WriteLine("  Boa Tarde!");
+            }
+            else
+            {
+                Console.WriteLine("  Boa Noite!");
+            }
+        }
+
+    }
+
+}
