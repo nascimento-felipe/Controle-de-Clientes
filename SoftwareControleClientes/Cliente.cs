d3801456e2948187aadf48c6f05bd346e8ad0fed
Felipe <nascimentofelipe565@gmail.com>
2021-10-01 22:57:56 -0300

    clients insertion done

--- SoftwareControleClientes/Cliente.cs
+++ SoftwareControleClientes/Cliente.cs
@@ -9,7 +9,7 @@ namespace SoftwareControleClientes
         private String nome;
         private int idade;
         private int codigo;
-        private String senha;
+        private StringBuilder senha = new StringBuilder();
 
 
         public void setIdade(int idade)
@@ -28,16 +28,40 @@ namespace SoftwareControleClientes
         {
             return idade; 
         }
-        public void setCodigo(int id)
-        { 
-            codigo = id; 
-        }
-        public int getCodigo()
-        {
-            return codigo;
-        }
-
+        public int Codigo { get => codigo; set => codigo = value; }
         public string Nome { get => nome; set => nome = value; }
-        public string Senha { get => senha; set => senha = value; }
+        public StringBuilder Senha { get => senha; set => senha = value; }
+
+        public void CadastrarCliente(int cod)
+        {
+            Console.WriteLine("  Seja bem vindo!\n"
+                + "    Digite o seu nome: ");
+            nome = Console.ReadLine().Trim();
+            Console.Clear();
+
+            Console.WriteLine("    Digite sua idade:");
+            setIdade(int.Parse(Console.ReadLine()));
+            Console.Clear();
+
+            Console.WriteLine("    Digite uma senha bem segura: ");
+            
+            while (true)
+            {
+                var key = Console.ReadKey(true);
+                
+                if (key.Key == ConsoleKey.Enter)
+                    break;
+
+                if (key.Key == ConsoleKey.Backspace && senha.Length > 0)
+                {
+                    senha.Remove(senha.Length - 1, 1);
+                }
+
+                else if(key.Key != ConsoleKey.Backspace) senha.Append(key.KeyChar);
+            }
+            Console.Clear();
+
+            codigo = cod;
+        }
     }
 }
--- SoftwareControleClientes/Program.cs
+++ SoftwareControleClientes/Program.cs
@@ -7,10 +7,9 @@ namespace SoftwareControleClientes
         static void Main(string[] args)
         {
             int hour = DateTime.Now.Hour;
-            String[] clientes;
+            string[] clientes = new string[10];
+            int i = 0;
             int opcao;
-
-            Cliente novoCliente = new Cliente();
 
             // Controle da mensagem de boas vindas.
             SendWelcomeMessage(hour);
@@ -20,14 +19,34 @@ namespace SoftwareControleClientes
                 Console.WriteLine("  Escolha uma opção:\n"
                     + "    1. Cadastrar novo cliente\n"
                     + "    2. Ver todos os clientes\n"
-                    + "    3. Deletar Clienten\n"
+                    + "    3. Deletar Cliente\n"
                     + "    4. Alterar Cliente\n"
                     + "   -1. Sair.");
 
                 opcao = int.Parse(Console.ReadLine());
                 if (opcao != -1)
                 {
+                    Console.Clear();
 
+                    switch (opcao)
+                    {
+                        case 1:
+                            Cliente novoCliente = new Cliente();
+                            novoCliente.CadastrarCliente(i);
+
+                            clientes.SetValue($"{novoCliente.Nome}, {novoCliente.Senha}, {novoCliente.getIdade()}", i);
+
+                            Console.WriteLine($"Cadastro concluído. Bem vindo(a), {novoCliente.Nome}!\n\n\n(senha: {novoCliente.Senha})");
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        default:
+                            break;
+                    }
                 }
             } while (opcao != -1);
         }
